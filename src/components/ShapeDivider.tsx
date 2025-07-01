e4e0a344b9d899c0b3cb4e0da8f7b2b146fab55d
itspokchop93 <barnes@ajjak.com>
2025-06-30 18:42:39 -0700

    dfghs

--- src/components/ShapeDivider.tsx
+++ src/components/ShapeDivider.tsx
@@ -2,21 +2,22 @@ import React from "react";
 
 export default function ShapeDivider() {
   return (
-    <div className="relative w-full h-32 overflow-hidden">
+    <div className="relative w-full h-40 overflow-hidden">
       {/* Base layer - darkest */}
       <svg
         className="absolute inset-0 w-full h-full"
-        viewBox="0 0 1440 320"
+        viewBox="0 0 1440 400"
         preserveAspectRatio="none"
+        style={{ width: '100%', height: '100%' }}
       >
         <defs>
           <linearGradient id="shapeGradient1" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.3" />
-            <stop offset="100%" stopColor="#ec4899" stopOpacity="0.3" />
+            <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.4" />
+            <stop offset="100%" stopColor="#ec4899" stopOpacity="0.4" />
           </linearGradient>
         </defs>
         <path
-          d="M0,64 C240,120 480,40 720,80 C960,120 1200,40 1440,80 L1440,240 C1200,200 960,280 720,240 C480,200 240,280 0,240 Z"
+          d="M-100,50 C120,200 360,20 600,150 C840,280 1080,60 1320,180 C1440,220 1540,100 1540,100 L1540,300 C1440,350 1320,250 1080,320 C840,250 600,380 360,300 C120,220 -100,350 -100,350 Z"
           fill="url(#shapeGradient1)"
         />
       </svg>
@@ -24,17 +25,18 @@ export default function ShapeDivider() {
       {/* Middle layer */}
       <svg
         className="absolute inset-0 w-full h-full"
-        viewBox="0 0 1440 320"
+        viewBox="0 0 1440 400"
         preserveAspectRatio="none"
+        style={{ width: '100%', height: '100%' }}
       >
         <defs>
           <linearGradient id="shapeGradient2" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.5" />
-            <stop offset="100%" stopColor="#ec4899" stopOpacity="0.5" />
+            <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.6" />
+            <stop offset="100%" stopColor="#ec4899" stopOpacity="0.6" />
           </linearGradient>
         </defs>
         <path
-          d="M0,96 C360,160 720,80 1080,120 C1260,160 1440,120 1440,120 L1440,200 C1260,160 1080,240 720,200 C360,160 0,240 0,200 Z"
+          d="M-100,80 C200,250 480,40 720,200 C960,360 1200,80 1440,220 C1540,280 1540,280 1540,280 L1540,320 C1440,260 1200,380 960,280 C720,180 480,340 200,240 C-100,140 -100,380 -100,380 Z"
           fill="url(#shapeGradient2)"
         />
       </svg>
@@ -42,17 +44,18 @@ export default function ShapeDivider() {
       {/* Top layer - brightest */}
       <svg
         className="absolute inset-0 w-full h-full"
-        viewBox="0 0 1440 320"
+        viewBox="0 0 1440 400"
         preserveAspectRatio="none"
+        style={{ width: '100%', height: '100%' }}
       >
         <defs>
           <linearGradient id="shapeGradient3" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.8" />
-            <stop offset="100%" stopColor="#ec4899" stopOpacity="0.8" />
+            <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.9" />
+            <stop offset="100%" stopColor="#ec4899" stopOpacity="0.9" />
           </linearGradient>
         </defs>
         <path
-          d="M0,128 C480,192 960,112 1440,152 L1440,168 C960,128 480,208 0,168 Z"
+          d="M-100,120 C240,300 480,60 720,240 C960,420 1200,100 1440,260 C1540,320 1540,320 1540,320 L1540,280 C1440,220 1200,380 960,280 C720,180 480,340 240,240 C-100,140 -100,140 -100,140 Z"
           fill="url(#shapeGradient3)"
         />
       </svg>
@@ -60,17 +63,18 @@ export default function ShapeDivider() {
       {/* Additional accent layer */}
       <svg
         className="absolute inset-0 w-full h-full"
-        viewBox="0 0 1440 320"
+        viewBox="0 0 1440 400"
         preserveAspectRatio="none"
+        style={{ width: '100%', height: '100%' }}
       >
         <defs>
           <linearGradient id="shapeGradient4" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.6" />
-            <stop offset="100%" stopColor="#ec4899" stopOpacity="0.6" />
+            <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.7" />
+            <stop offset="100%" stopColor="#ec4899" stopOpacity="0.7" />
           </linearGradient>
         </defs>
         <path
-          d="M0,80 C180,40 360,120 540,80 C720,40 900,120 1080,80 C1260,40 1350,60 1440,80 L1440,240 C1350,220 1260,260 1080,240 C900,220 720,260 540,240 C360,220 180,260 0,240 Z"
+          d="M-100,100 C180,10 360,280 540,120 C720,20 900,300 1080,140 C1260,40 1380,200 1540,160 L1540,360 C1380,320 1260,380 1080,320 C900,260 720,400 540,340 C360,280 180,410 -100,360 Z"
           fill="url(#shapeGradient4)"
         />
       </svg>
