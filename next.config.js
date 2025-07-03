f4ce185f97bb610a3845bc23a921ee2d177a0b2c
itspokchop93 <barnes@ajjak.com>
2025-07-02 20:32:01 -0700

    fix: relax CSP for /iframe-communicator.html to allow inline scripts/styles for Authorize.net Accept Hosted

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ["i.scdn.co", "image-cdn-ak.spotifycdn.com"],
   },
+  async headers() {
+    return [
+      {
+        source: '/iframe-communicator.html',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
+          }
+        ]
+      }
+    ];
+  },
 };
 
 module.exports = nextConfig; 
